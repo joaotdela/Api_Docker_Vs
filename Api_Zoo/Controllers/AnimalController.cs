7c2e3391e476e57d4e59ae21fcbb19d75dc156d0
joaotdela <67844082+joaotdela@users.noreply.github.com>
2021-09-22 19:48:19 -0300

    animals part

---
+++ Api_Zoo/Controllers/AnimalController.cs
@@ -0,0 +1,58 @@
+﻿using Api_Zoo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Zoo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+
+
+
+        // GET: api/<AnimalController>
+        [HttpGet]
+        public List<Animal> Get()
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Animal(1, "Leão", "", ""));
+            animals.Add(new Animal(2, "Tigre", "", ""));
+            animals.Add(new Animal(3, "Crodilo", "", ""));
+            animals.Add(new Animal(4, "Elefante", "", ""));
+            animals.Add(new Animal(5, "Girafa", "", ""));
+            animals.Add(new Animal(6, "Macaco", "", ""));
+
+            return animals;
+        }
+
+        // GET api/<AnimalController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AnimalController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AnimalController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AnimalController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api_Zoo/Models/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Zoo.Models
+{
+    public class Animal
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string classname { get; set; }
+        public string descri { get; set; }
+        public Animal()
+        {
+
+        }
+        public Animal(int id, string name, string classname, string descri)
+        {
+            this.id = id;
+            this.name = name;
+            this.classname = classname;
+            this.descri = descri;
+        }
+    }
+}
